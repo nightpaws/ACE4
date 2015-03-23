cc2b1f70927f6716cf94ec6edcd343817d5ad1e7
Nightpaws <nightpaws@users.noreply.github.com>
2015-03-23 19:32:24 +0000

    Redesigned initial thread creation code to use thread pool server
    Added beginnings of code to accept open input stream in preparation to
    receive HTTP requests.

--- src/HttpRequest.java
+++ src/HttpRequest.java
@@ -1,9 +1,51 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
 final class HttpRequest implements Runnable {
+
+	final static String CRLF = "\r\n";
+	Socket socket;
+
+	// Constructor
+	public HttpRequest(Socket socket) throws Exception {
+		this.socket = socket;
+	}
 
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-		
+		try {
+			processRequest();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 	}
+
+	private void processRequest() throws Exception {
+		// Get a reference to the socket's input and output streams.
+		InputStream is =  this.socket.getInputStream();
+		DataOutputStream os = new DataOutputStream(this.socket.getOutputStream());
+		// Set up input stream filters.
+		// ?
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+		// Get the request line of the HTTP request message.
+		String requestLine = Something; //stuff needs to go here
+		// Display the request line.
+		System.out.println();
+		System.out.println(requestLine);
+		
+		
+		
+		
+	
+	
+
+	}
+
 	// . . .
 }
--- src/WebServer.java
+++ src/WebServer.java
@@ -6,18 +6,20 @@ public final class WebServer {
 	public static void main(String argv[]) throws Exception {
 		// Set the port number.
 		int port = 7000;
+		int poolSize = 10;
 		// Establish the listen socket.
 		ServerSocket sock = new ServerSocket(port);
+//		ExecutorService executorService = Executors.newCachedThreadPool();
+		ExecutorService serverThreadPool = Executors.newFixedThreadPool(poolSize);
 		System.out.println("Server is running!");
-		ExecutorService executorService = Executors.newCachedThreadPool();
-
-		
 		
 		// Process HTTP service requests in an infinite loop.
 		while (true) {
 			// Listen for a TCP connection request.
 			Socket client = sock.accept();
-			executorService.execute(new Server(client));
+//			executorService.execute(new Server(client));
+			Thread thread = new Thread(new HttpRequest(client));
+			serverThreadPool.execute(thread);
 			// Code below here will not execute
 		}
 		
