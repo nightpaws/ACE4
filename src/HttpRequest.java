11e8e94d900754286b1245e6af8cc85f226de4a5
Nightpaws <nightpaws@users.noreply.github.com>
2015-03-26 10:00:52 +0000

    Functional Web Server
    Functions on port 7000 and opens files, or displays a not found custom
    page.

--- src/HttpRequest.java
+++ src/HttpRequest.java
@@ -1,8 +1,12 @@
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.Socket;
+import java.util.StringTokenizer;
 
 final class HttpRequest implements Runnable {
 
@@ -26,11 +30,12 @@ final class HttpRequest implements Runnable {
 
 	private void processRequest() throws Exception {
 		// Get a reference to the socket's input and output streams.
-		InputStream is =  this.socket.getInputStream();
-		DataOutputStream os = new DataOutputStream(this.socket.getOutputStream());
+		InputStream is = this.socket.getInputStream();
+		DataOutputStream os = new DataOutputStream(
+				this.socket.getOutputStream());
 		// Set up input stream filters.
 		// ?
-		
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(is));
 
 		// Get the request line of the HTTP request message.
@@ -38,20 +43,97 @@ final class HttpRequest implements Runnable {
 		// Display the request line.
 		System.out.println();
 		System.out.println(requestLine);
-		
+
 		// Get and display the header lines.
 		String headerLine = null;
 		while ((headerLine = br.readLine()).length() != 0) {
-		 System.out.println(headerLine);
+			System.out.println(headerLine);
 		}
-		
+
+		// Extract the filename from the request line.
+		StringTokenizer tokens = new StringTokenizer(requestLine);
+		tokens.nextToken(); // skip over the method, which should be "GET"
+		String fileName = tokens.nextToken();
+		// Prepend a "." so that file request is within the current directory.
+		fileName = "." + fileName;
+
+		// Open the requested file.
+		FileInputStream fis = null;
+		boolean fileExists = true;
+
+		try {
+			fis = new FileInputStream(fileName);
+		} catch (FileNotFoundException e) {
+			fileExists = false;
+		}
+
+		// Construct the response message.
+		String statusLine = null;
+		String contentTypeLine = null;
+		String entityBody = null;
+		if (fileExists) {
+			System.out.println("---- File Found ----");
+			statusLine = "HTTP/1.1 200 OK"; // edit
+			contentTypeLine = "Content-type: " + contentType(fileName) + CRLF;
+			System.out.println("-------------------");
+
+		} else {
+			System.out.println("---- File Not Found ----");
+			statusLine = "HTTP/1.1 404 Not Found"; // edit
+			contentTypeLine = "Content-type: text/html"+ CRLF;
+			entityBody = "<HTML>" + "<HEAD><TITLE>Not Found</TITLE></HEAD>"
+					+ "<BODY>Not Found</BODY></HTML>";
+			System.out.println("-------------------");
+
+		}
+
+		// Send the status line.
+		os.writeBytes(statusLine);
+		// Send the content type line.
+		os.writeBytes(contentTypeLine);
+		// Send a blank line to indicate the end of the header lines.
+		os.writeBytes(CRLF);
+
+		// Send the entity body.
+		if (fileExists) {
+			sendBytes(fis, os);
+			fis.close();
+		} else {
+			os.writeBytes(entityBody);
+		}
+
 		// Close streams and socket.
 		os.close();
 		br.close();
 		socket.close();
-	
-	
 
+	}
+
+	private static String contentType(String fileName) {
+		if (fileName.endsWith(".htm") || fileName.endsWith(".html")) {
+			return "text/html";
+		}
+		if (fileName.endsWith(".gif")) {
+			return "image/gif";
+		}
+		if (fileName.endsWith(".jpg") || fileName.endsWith(".jpeg")) {
+			return "image/jpeg";
+		}
+		if (fileName.endsWith("png")) {
+			return "image/png";
+		}
+		return "application/octet-stream";
+	}
+
+	private static void sendBytes(FileInputStream fis, OutputStream os)
+			throws Exception {
+		// Construct a 1K buffer to hold bytes on their way to the socket.
+		byte[] buffer = new byte[1024];
+		int bytes = 0;
+		// Copy requested file into the socket's output stream.
+		while ((bytes = fis.read(buffer)) != -1) {
+			os.write(buffer, 0, bytes);
+		}
 	}
 
 	// . . .
